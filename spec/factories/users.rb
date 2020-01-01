2bbae639865f8249c934b3269ad5305b2d338bab
ViriCruz <viridiana.cruz.olivera@gmail.com>
2020-01-01 08:38:41 -0700

    Add Faker generator email

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,20 @@
+require 'factory_bot'
 FactoryBot.define do
-    factory :user do
-      id {}            
+    factory :user do            
       first_name {"Ignacio"}
       last_name {"Loyola"}
-      email {"psdfgfh@example.com"}
+      email {Faker::Internet.safe_email}
       phone_number {"0303456"}
       password {"laralala"}      
     end
-  
+
+    factory :random_user, class: User do
+      first_name { Faker::Name.first_name }
+      last_name { Faker::Name.last_name }
+      email { Faker::Internet.safe_email }
+      phone_number {"5555555555"}
+      password {"qwerty"}
+    end
     # Not used in this tutorial, but left to show an example of different user types
     # factory :admin do
     #   id {2}
@@ -15,5 +22,5 @@ FactoryBot.define do
     #   password {"qwerty"}
     #   admin {true}
     # end
-  end
+end
   
