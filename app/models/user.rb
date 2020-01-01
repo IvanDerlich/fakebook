da5f28b044cf5d4c7e4bda8b92c7e49ab5deb8aa
ViriCruz <viridiana.cruz.olivera@gmail.com>
2020-01-01 09:23:11 -0700

    Add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+  validates :first_name, :last_name, :email, :phone_number, presence: true
+  validates :first_name, length: { minimum: 2 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
   has_many :posts
 end
