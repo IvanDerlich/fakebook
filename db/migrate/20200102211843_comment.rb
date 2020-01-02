9916b930be1100aa35cf0725ccb4847d0a8aebbc
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-01-02 18:39:40 -0300

    Add comment migration

---
+++ db/migrate/20200102211843_comment.rb
@@ -0,0 +1,10 @@
+class Comment < ActiveRecord::Migration[6.0]
+  def change
+    create_table :comments do |t|
+      t.references :post, foreign_key: true
+      t.references :user, foreign_key: true
+      t.string :content
+      t.timestamps
+    end
+  end
+end
