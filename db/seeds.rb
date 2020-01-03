0a716db7745463845038370d6db67416ef9c7e0c
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-01-02 21:11:16 -0300

    Seed 50 posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,25 +30,12 @@ user.posts.create!(
     )
   end
   
-#   users = User.order(:created_at).take(6)
+   users = User.order(:created_at).take(6)
   
-#   50.times do
-#     description = Faker::Superhero.name+"'s promotion to "+Faker::Military.air_force_rank
-#     users.each do |u|
-#       u.events.create!(
-#         description: description,
-#         date: Time.now
-#       )
-#     end
-#   end
-  
-#   events = Event.order(:created_at).take(6)
-  
-#   events.each do |e|
-#     users.each do |u|
-#       Attendance.create!(
-#         attended_event_id: e.id,
-#         attendee_id: u.id      
-#       )
-#     end
-#   end
+  50.times do
+    users.each do |u|
+      u.posts.create!(
+        text: Faker::Lorem.paragraph(sentence_count: 2)
+      )
+    end
+  end
