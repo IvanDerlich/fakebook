586c6215717f5800bd7f431ec70023ffa68fa238
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-01-03 19:26:34 -0300

    Fix some test names

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -10,27 +10,27 @@ RSpec.describe Comment, type: :model do
         expect(comment).to be_valid
     end
 
-    it '# invalid comment with no text ' do
+    it '# creates an invalid comment with no text ' do
         comment.text = nil
         expect(comment).to_not be_valid
     end
 
-    it '# invalid blank comment' do
+    it '# creates an invalid blank comment' do
         comment.text = ""
         expect(comment).to_not be_valid
     end
 
-    it '# invalid comment with text longer than 300 characters ' do
+    it '# creates an invalid comment with text longer than 300 characters ' do
         comment.text = Faker::Lorem.paragraph_by_chars(number: 301, supplemental: false)
         expect(comment).to_not be_valid
     end    
 
-    it '# invalid postless comment' do
+    it '# creates an invalid postless comment' do
         comment.post = nil
         expect(comment).to_not be_valid
     end
 
-    it '# userless comment ' do
+    it '# creates an invalid userless comment ' do
         comment.user = nil
         expect(comment).to_not be_valid
     end   
