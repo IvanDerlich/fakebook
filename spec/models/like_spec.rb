d5a77e666a97e5e0c98eb2d158f5c53e90cb48a5
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-01-03 19:26:57 -0300

    Add Likes test

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,16 +2,27 @@ require 'rails_helper'
 
 RSpec.describe Like, type: :model do 
 
-    xit 'invalid like that belongs to user but not to a post' do
+    let(:like) {
+        FactoryBot.create(:like)
+    }
+
+    it '# creates a valid like' do
+        expect(like).to be_valid
     end
 
-    xit 'invalid like that belongs to post but not to a user ' do
+    it '# invalid like that belongs to user but not to a post' do
+        like.post = nil
+        expect(like).to_not be_valid
     end
 
-    xit 'invalid like that neither to a post nor to a user ' do
+    it '# invalid like that belongs to post but not to a user ' do
+        like.user = nil
+        expect(like).to_not be_valid
     end
 
-    xit 'creates a valid like' do
-    end
+    it '# invalid like that neither to a post nor to a user ' do
+        like.post = like.user = nil
+        expect(like).to_not be_valid
+    end   
 
 end
