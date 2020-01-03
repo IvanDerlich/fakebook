2c9f73aaeb1ae46a65d9d89e9495e62aafd13d44
ViriCruz <viridiana.cruz.olivera@gmail.com>
2020-01-03 13:34:38 -0700

    Change indentation spaces to 2

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
-    validates :text, :user_id, presence: true    
-    validates :text, length: { maximum: 300 }
-  end
+  belongs_to :user
+  belongs_to :post
+  validates :text, :user_id, presence: true    
+  validates :text, length: { maximum: 300 }
+end
