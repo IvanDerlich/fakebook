a5e4e6f94910569c11bc4ca372b0fc7b8beee334
ViriCruz <viridiana.cruz.olivera@gmail.com>
2020-01-03 12:39:37 -0700

    Lower the minimum length for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@
 class Post < ApplicationRecord
   belongs_to :user
   validates :text, :user_id, presence: true
-  validates :text, length: { in: 15..300 }
+  validates :text, length: { in: 5..300 }
 end
