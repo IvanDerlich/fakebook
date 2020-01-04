ff2c8b42623165364609b989f5ad697424b9315b
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-01-04 02:57:44 -0300

    Pass test for feauture 'User comments Post'

--- spec/features/user_comments_post_spec.rb
+++ spec/features/user_comments_post_spec.rb
@@ -2,22 +2,41 @@
 
 require 'rails_helper'
 
-RSpec.describe 'User comments post/s', type: :feature do
+RSpec.describe 'User-Comments-Post', type: :feature do
+
+	let(:post) { FactoryBot.create(:random_post)}
+	let(:user) { FactoryBot.create(:random_user)}
+	let(:users) { FactoryBot.create_list(:random_user,5) }
+	let(:posts) { FactoryBot.create_list(:random_post,5) }
+
 
 	scenario '# user comments one post' do
-
+		comment = user.comments_a_post(
+			post, 			
+			Faker::Lorem.paragraph_by_chars(number: 40, supplemental: false)
+		)
+		expect(comment).to be_valid
 	end
 
-	scenario '# user comments 5 posts' do
-
-  end
-  
-  scenario '# post gets one comment' do
+	scenario '# user comments 5 different posts' do
+		posts.each{ |item|
+			comment = user.comments_a_post item, Faker::Lorem.paragraph_by_chars(number: 20, supplemental: false)
+			expect(comment).to be_valid
+		}
+	end
 	
+	scenario '# post gets 5 comments from 5 different users' do
+		users.each{ |item|
+			comment = item.comments_a_post post, Faker::Lorem.paragraph_by_chars(number: 20, supplemental: false)
+			expect(comment).to be_valid
+		}
 	end
 
-	scenario '# post gests 5 comments' do
-
+	scenario '# post gets 5 comments from one user' do
+		5.times do
+			comment = user.comments_a_post post, Faker::Lorem.paragraph_by_chars(number: 20, supplemental: false)
+			expect(comment).to be_valid
+		end
 	end	
 
 end
