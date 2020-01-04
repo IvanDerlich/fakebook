3196361f3648ee093668d392f7a33a8293d9ca52
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-01-04 02:56:49 -0300

    Add User and Edit Post factories

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,10 +1,11 @@
 require 'factory_bot'
 FactoryBot.define do
 
-    factory :post do            
-      text {Faker::Lorem.paragraph_by_chars(number: 30, supplemental: false)}      
-      association :user, factory: :user      
+    factory :random_post, class: Post do            
+      #text { "asdfasdfgsdfgsdfgsdf" }
+      text { Faker::Lorem.paragraph_by_chars(number: 30, supplemental: false) }      
+      association :user, factory: :random_user      
     end
-    
+
 end
   
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 require 'factory_bot'
 FactoryBot.define do
 
-  factory :user, class: User do
+  factory :random_user, class: User do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     email { Faker::Internet.safe_email }
