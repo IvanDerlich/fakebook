471845d68d1c2f09ed3ddaccdfe32d8327718ff5
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-01-06 18:46:40 -0300

    Add custom validation to confirmed attribute of friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,13 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, :class_name => "User"
+  validate :false_but_not_nil
+
+  def false_but_not_nil
+    if confirmed.nil?
+      errors.add :confirmed, 'confirmed cannot be nil'
+    end
+  end
+
+
 end
