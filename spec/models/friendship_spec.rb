015ef279473a8e5f77710718370aa94e2a0ad384
ViriCruz <viridiana.cruz.olivera@gmail.com>
2020-01-06 12:57:25 -0700

    Refactor tests

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -3,10 +3,21 @@ require 'rails_helper'
 RSpec.describe Friendship, type: :model do  
   xit "# send a request" do
   end
+
+  xit "# see all requests sent" do
+    
+  end
+  xit "# see all requests received" do
+    
+  end
+
   xit "# see all requests sent and requests received" do
   end 
+
+  # 6th milestone
   xit "# user sends invalid request to itself" do
   end
+
   xit "# user sends invalid request twice to another user" do
   end
 end
