043e9eeef55b86af9fca63023da5de8a302a093e
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-01-06 18:17:59 -0300

    Create friendship test file

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,12 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do  
-  xit "# send a request" do
+
+  let(:unconfirmed_frienship) { FactoryBot.create(:unconfirmed_frienship) }
+  let(:confirmed_frienship) { FactoryBot.create(:confirmed_frienship) }
+
+  it '# creates a valid unconfirmed_friendship' do
+      expect(unconfirmed_friendship).to be_valid
   end
-  xit "# see all requests sent and requests received" do
-  end 
-  xit "# user sends invalid request to itself" do
+
+  it '# creates a valid confirmed_friendship' do
+    expect(confirmed_friendship).to be_valid
   end
-  xit "# user sends invalid request twice to another user" do
+
+  xit '# invalid like that belongs to user but not to a post' do
+      like.post = nil
+      expect(like).to_not be_valid
   end
+
+  xit '# invalid like that belongs to post but not to a user ' do
+      like.user = nil
+      expect(like).to_not be_valid
+  end
+
+  xit '# invalid like that neither has a post nor a user ' do
+      like.post = like.user = nil
+      expect(like).to_not be_valid
+  end  
+
 end
