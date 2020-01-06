3bc51eec7560e4e06f1ff43bc6a98da244943453
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-01-06 18:16:35 -0300

    Add friendships factory

--- spec/factories/friendships.rb
+++ spec/factories/friendships.rb
@@ -1,7 +1,15 @@
 FactoryBot.define do
-  factory :friendship do
-    user { nil }
-    friend { nil }
+
+  factory :unconfirmed_friendship, class: Friendship do
+    association :user, factory: :random_user     
+    association :friend, factory: :random_user     
     confirmed { false }
   end
+
+  factory :confirmed_friendship, class: Friendship do
+    association :user, factory: :random_user     
+    association :friend, factory: :random_user     
+    confirmed { true }
+  end
+
 end
