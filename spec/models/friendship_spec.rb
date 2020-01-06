910662ea129d5a9e6ba58fcb7cccf5d579fd790a
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-01-06 16:37:17 -0300

    Add friendship model, test and migration

---
+++ spec/models/friendship_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Friendship, type: :model do  
+  xit "# send a request" do
+  end
+  xit "# see all requests sent and requests received" do
+  end 
+  xit "# user sends invalid request to itself" do
+  end
+  xit "# user sends invalid request twice to another user" do
+  end
+end
