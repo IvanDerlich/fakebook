d681c3bfc81d368640bedcee76f4161753201614
ViriCruz <viridiana.cruz.olivera@gmail.com>
2020-01-07 10:38:40 -0700

    Add new action to create a new a friendship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,9 @@
 class FriendshipsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+  
   def new
-    user = User.second
-    friendship = current_user.request_friendship(user)
+    friend = User.find_by(id: params[:user])
+    friendship = current_user.request_friendship(friend)
     flash[:success] = friendship.save ? "You sent a request successfully" : "Some error while sending"
     redirect_to users_path
   end
