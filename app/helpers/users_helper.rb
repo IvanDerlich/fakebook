a3729d5154f98493085a5dbfca5848b51af43eea
ViriCruz <viridiana.cruz.olivera@gmail.com>
2020-01-07 10:40:49 -0700

    Add friendship_pending? method to set friendship status text

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,19 +2,7 @@
 
 module UsersHelper
 
-  def some_text(friendships, friend)
-    message = nil
-    friendships.each do |f|
-      p f
-    end
-    if friendships.include?(friend)
-      index = friendships.index(friend)
-      message = friendships[index].confirmed ? "Friends" : "Pending"
-      p "entro al if"
-    else
-      p "entro al else"
-      message = "Send Friendship Request"
-    end
-    return "send"
+  def friendship_pending?(friend)
+   @unconfirmed_sent_friendships.include?(friend)
   end
 end
