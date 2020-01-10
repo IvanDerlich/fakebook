dce8356f7634ce441d4a2397b56b45ce4017f50b
ViriCruz <viridiana.cruz.olivera@gmail.com>
2020-01-10 16:59:55 -0700

    Add update action to confirm friendship.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,10 +1,16 @@
 class FriendshipsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :update]
   
   def new
     friend = User.find_by(id: params[:user])
     friendship = current_user.requests_friendship(friend)
     flash[:success] = friendship.save ? "You sent a request successfully" : "Some error while sending"
+    redirect_to users_path
+  end
+
+  def update
+    friend = User.find_by(id: params[:friend_id])
+    flash[:message] = current_user.confirms_friendship(friend) ? "You became friends!" : "Something went wrong :("
     redirect_to users_path
   end
 end
