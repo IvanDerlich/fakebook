9f913654b9a3ecaa9f97b78f66a459f81f4c52f5
ViriCruz <viridiana.cruz.olivera@gmail.com>
2020-01-10 17:01:53 -0700

    Refactor current user's unconfirmed sent friendships. Add request friendships received list.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@
 
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: %i[show index]
+
   def show
     @posts = current_user.posts.order('created_at desc')
     @comment = Comment.new
@@ -9,6 +10,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    @unconfirmed_sent_friendships = current_user.unconfirmed_sent_friendships
+    @unconfirmed_sent_friendships = current_user.requests_sent
+    @requests_received = current_user.requests_received
   end
 end
