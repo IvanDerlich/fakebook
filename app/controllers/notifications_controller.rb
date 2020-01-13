ef48abdf27881b0490b63cd64beb505d2b28a614
ViriCruz <viridiana.cruz.olivera@gmail.com>
2020-01-13 15:59:27 -0700

    Add sent requests and received requests array objects to show action.

---
+++ app/controllers/notifications_controller.rb
@@ -0,0 +1,8 @@
+class NotificationsController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+  
+  def show
+    @received_requests = current_user.requests_received
+    @sent_requests = current_user.requests_sent
+  end
+end
