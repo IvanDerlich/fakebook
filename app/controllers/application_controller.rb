54f80057c9a57faac88987cb584170dd3fff137a
ViriCruz <viridiana.cruz.olivera@gmail.com>
2020-01-14 15:24:13 -0700

    Add protect from forgery.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
