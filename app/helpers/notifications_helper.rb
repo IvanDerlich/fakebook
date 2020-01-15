b39542eb27e43fc34902582b12984d5f21485f8d
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-01-15 02:26:57 -0300

    Correct rubocop offenses

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 module NotificationsHelper
-  def friend_requests_count 
-    if current_user.requests_received.count != 0
-      return current_user.requests_received.count
-    end    
+  def friend_requests_count
+    return current_user.requests_received.count if current_user.requests_received.count != 0
+
     0
   end
 end
